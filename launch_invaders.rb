25d05bb870391f43fbd0728c33bb2874a092498e
Adam Sheehan <a.t.sheehan@gmail.com>
2013-12-02 20:38:22 -0500

    A single invader descends slowly.

--- launch_invaders.rb
+++ launch_invaders.rb
@@ -1,16 +1,37 @@
 #!/usr/bin/env ruby
 require 'gosu'
+require_relative 'world'
 
 class LaunchInvaders < Gosu::Window
+  SCREEN_WIDTH = 640
+  SCREEN_HEIGHT = 480
+
   def initialize
-    super(640, 480, false)
+    super(SCREEN_WIDTH, SCREEN_HEIGHT, false)
     self.caption = 'Launch Invaders'
+
+    @world = World.new(SCREEN_WIDTH, SCREEN_HEIGHT, 10)
   end
 
   def update
+    @world.update
   end
 
   def draw
+    @world.invaders.each do |invader|
+      draw_rect(invader.bounds, Gosu::Color::WHITE)
+    end
+  end
+
+  private
+
+  def draw_rect(rect, color)
+    draw_quad(
+      rect.left, rect.top, color,
+      rect.left, rect.bottom, color,
+      rect.right, rect.top, color,
+      rect.right, rect.bottom, color
+    )
   end
 end
 
---
+++ world.rb
@@ -0,0 +1,71 @@
+class World
+  attr_reader :invaders
+
+  def initialize(width, height, invader_count)
+    @width = width
+    @height = height
+
+    @invaders = []
+    @invaders << Invader.new(width)
+  end
+
+  def update
+    @invaders.each do |invader|
+      invader.update()
+    end
+  end
+end
+
+class Invader
+  def initialize(max_width)
+    @pos = Vec2D.new(100, 20)
+  end
+
+  def size
+    Vec2D.new(20, 20)
+  end
+
+  def bounds
+    Rect.new(@pos, size)
+  end
+
+  def update
+    @pos = @pos.add(0, 1)
+  end
+end
+
+class Rect
+  def initialize(origin, size)
+    @origin = origin
+    @size = size
+  end
+
+  def top
+    @origin.y
+  end
+
+  def bottom
+    @origin.y + @size.y
+  end
+
+  def left
+    @origin.x
+  end
+
+  def right
+    @origin.x + @size.x
+  end
+end
+
+class Vec2D
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def add(x, y)
+    Vec2D.new(@x + x, @y + y)
+  end
+end
