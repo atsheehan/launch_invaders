a1e28506c74d78e1904b8b7a9d7961e835d66807
Adam Sheehan <a.t.sheehan@gmail.com>
2013-12-02 20:46:07 -0500

    Multiple invaders are spawned at random intervals.

--- world.rb
+++ world.rb
@@ -4,12 +4,22 @@ class World
   def initialize(width, height, invader_count)
     @width = width
     @height = height
-
+    @invaders_remaining = invader_count
+    @spawn_delay = 0
     @invaders = []
-    @invaders << Invader.new(width)
   end
 
   def update
+    if @invaders_remaining > 0
+      if @spawn_delay <= 0
+        @invaders << Invader.new(@width)
+        @spawn_delay = rand(100)
+        @invaders_remaining -= 1
+      else
+        @spawn_delay -= 1
+      end
+    end
+
     @invaders.each do |invader|
       invader.update()
     end
